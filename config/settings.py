8b79be467aecc075a523747c2f1b40ea5f6cb39f
Taehoon Park <memune@Taehoonui-MacBookPro.local>
2021-09-12 22:34:53 +0900

    class 11

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
