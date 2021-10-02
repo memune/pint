0813c0614fd663768aa8573d065fc9755cfffe4e
Taehoon Park <memune@Taehoonui-MacBookPro.local>
2021-10-02 22:57:58 +0900

    update view error resolve

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, DeleteView
+from django.views.generic import CreateView, DetailView, DeleteView, UpdateView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -12,19 +12,24 @@ from accountapp.models import HelloWorld
 
 def hello_world(request):
 
-    if request.method == "POST":
+    if request.user.is_authenticated:
 
-        temp = request.POST.get('hello_world_input')
+        if request.method == "POST":
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+            temp = request.POST.get('hello_world_input')
 
-        hello_world_list = HelloWorld.objects.all()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+
+            hello_world_list = HelloWorld.objects.all()
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 class AccountCreateView(CreateView):
@@ -39,17 +44,43 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-class AccountUpdateView(CreateView):
+class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     # reverse_lazy는 cbv에서 활용
     template_name = 'accountapp/update.html'
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return super().post(*args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     # reverse_lazy는 cbv에서 활용
     template_name = 'accountapp/delete.html'
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return super().post(*args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
 
 
