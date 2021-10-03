4fea8e59c2d06071b45aadaa7dae62fa50bca76e
Taehoon Park <memune@Taehoonui-MacBookPro.local>
2021-10-03 10:54:39 +0900

    lesson 31

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+
+    # 이 부분을 제대로 이해하지 못함
+    # user 데이터가 왜 제대로 넘어와지지 않는 것인지 그 부분을 이해 못함
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        return super().form_valid(form)
