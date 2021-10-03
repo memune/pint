f5d37303d58131133d2594043ea688a1c6783b72
Taehoon Park <memune@Taehoonui-MacBookPro.local>
2021-10-03 17:29:46 +0900

    lesson 33

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
+
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -11,9 +14,7 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
-
 
     # 이 부분을 제대로 이해하지 못함
     # user 데이터가 왜 제대로 넘어와지지 않는 것인지 그 부분을 이해 못함
@@ -22,3 +23,18 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'get')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
