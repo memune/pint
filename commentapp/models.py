67bab21ded3b9fa44d8d03e1a9c144fb579fec24
Taehoon Park <memune@Taehoonui-MacBookPro.local>
2021-10-09 17:22:11 +0900

    lesson 38

--- commentapp/models.py
+++ commentapp/models.py
@@ -7,7 +7,7 @@ from articleapp.models import Article
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True, related_name='comment')
-    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='related_name')
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='comment')
 
     content = models.TextField(null=True)
 
