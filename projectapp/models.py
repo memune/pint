9de1fea548222350887945703bd64985ec3e1640
Taehoon Park <memune@Taehoonui-MacBookPro.local>
2021-10-10 14:49:36 +0900

    lesson 40

---
+++ projectapp/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='project', null=True)
+
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField(null=True)
+    image = models.ImageField(upload_to='project/', null=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+
